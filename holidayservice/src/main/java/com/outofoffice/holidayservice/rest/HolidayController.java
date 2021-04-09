2cb8055b5c1394975656b0e5658223f5274f5390
dalilacorovic <dcorovic2@etf.unsa.ba>
2021-04-09 19:03:41 +0200

    Adding route for leaverequest ms

--- holidayservice/src/main/java/com/outofoffice/holidayservice/rest/HolidayController.java
+++ holidayservice/src/main/java/com/outofoffice/holidayservice/rest/HolidayController.java
@@ -34,17 +34,12 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping(value = "/")
 public class HolidayController {
-	@Autowired
-	RestTemplate restTemplate; 
-	
 	private final HolidayService holidayService;
-	final String uri = "http://employee-service/getAllEmployeesNames";
-
+	
 	public HolidayController(HolidayService holidayService) {
 		this.holidayService = holidayService;
 	}
-	
-	
+		
 	@ApiOperation(value = "Admin creates a holiday with empty list of employees")
 	@PostMapping(value = "/holiday/{holidayTypeID}")
 	public ResponseEntity<?> insertHoliday(@RequestBody @Validated HolidayRequest requestHoliday, @PathVariable long holidayTypeID, Errors errors) {
@@ -53,24 +48,26 @@ public class HolidayController {
 	
 	@ApiOperation(value = "This holiday applies to all employees")
 	@PostMapping(value = "/defaultHoliday/{holidayTypeID}")
-	public ResponseEntity<?> insertDefaultHoliday(@RequestBody @Validated HolidayRequest requestHoliday, @PathVariable long holidayTypeID, Errors errors) {
-		
-		HolidayResponse[] response =  restTemplate.getForObject(uri, HolidayResponse[].class);
-		List<HolidayResponse> sourceList = Arrays.asList(response);
-		
-		return holidayService.insertDefaultHoliday(requestHoliday, holidayTypeID, sourceList);
+	public ResponseEntity<?> insertDefaultHoliday(@RequestBody @Validated HolidayRequest requestHoliday, @PathVariable Long holidayTypeID, Errors errors) {
+		return holidayService.insertDefaultHoliday(requestHoliday, holidayTypeID);
 	}
 	
 	@ApiOperation(value = "Update holiday list - the employee chooses a holiday")
 	@PatchMapping(value = "/holiday/{holidayTypeID}/{employeeID}/{firstAndLastName}")
-	public ResponseEntity<?> updateListHoliday(@PathVariable long holidayTypeID, @PathVariable long employeeID, @PathVariable String firstAndLastName) {
+	public ResponseEntity<?> updateListHoliday(@PathVariable Long holidayTypeID, @PathVariable Long employeeID, @PathVariable String firstAndLastName) {
 		return holidayService.updateListHoliday(holidayTypeID, employeeID, firstAndLastName);
 	}
 	
 	@ApiOperation(value = "Update holiday list - admin modifies data")
 	@PatchMapping(value = "/holiday/{holidayId}")
-	public ResponseEntity<?> updateHoliday(@RequestBody @Validated HolidayRequest requestHoliday, @PathVariable long holidayId, Errors errors) {
+	public ResponseEntity<?> updateHoliday(@RequestBody @Validated HolidayRequest requestHoliday, @PathVariable Long holidayId, Errors errors) {
 		return holidayService.updateHoliday(requestHoliday, holidayId);
+	}
+	
+	@ApiOperation(value = "Update holiday - delete employee")
+	@PatchMapping(value = "/holidayupdate/{employeeId}")
+	public ResponseEntity<?> updateHolidayListOfEmployees(@PathVariable Long employeeId) {
+		return holidayService.updateHolidayListOfEmployees(employeeId);
 	}
 	
 	@ApiOperation(value = "Delete holiday using specific holiday id - admin", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -81,11 +78,17 @@ public class HolidayController {
 	
 	@ApiOperation(value = "Get list of employees for specific holiday type")
 	@GetMapping(value = "/getlistofemployees/{holidayTypeId}")
-	public ResponseEntity<?> getAllHolidayEmployeeList(@PathVariable long holidayTypeId) {
+	public ResponseEntity<?> getAllHolidayEmployeeList(@PathVariable Long holidayTypeId) {
 		return holidayService.getAllEmployees(holidayTypeId);
 	}
-
 	
+	@ApiOperation(value = "Get list of all holidays")
+	@GetMapping(value = "/getlistofholidays")
+	public ResponseEntity<?> getListOfHolidays() {
+		return holidayService.getAllHolidays();
+	}
+
+	@ApiOperation(value = "Check if the holiday is between leave dates")
 	@GetMapping(value = "/holiday/{startDate}/{daysNum}")
 	public ResponseEntity<?> getDaysNumOfHoliday(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate, @PathVariable int daysNum) {
 		return holidayService.getDaysNumOfHoliday(startDate, daysNum);
